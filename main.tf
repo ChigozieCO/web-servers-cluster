54c5663e59809fffba52f9bfea4c64b77e43b1d9
TheGozie <chiggy.nm@gmail.com>
2023-01-05 21:47:11 +0100

    Corrected format

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ provider "aws" {
 ## It's best to use a launch template but for this solution we would use a launch configuration.
 
 resource "aws_launch_configuration" "launchconfig" {
-  image_id            = "ami-0a6b2839d44d781b2"
-  instance_type       = "t2.micro"
+  image_id        = "ami-0a6b2839d44d781b2"
+  instance_type   = "t2.micro"
   security_groups = [aws_security_groups.server2-SG.id]
 
   user_data = <<-EOF
@@ -24,7 +24,7 @@ resource "aws_launch_configuration" "launchconfig" {
 
 resource "aws_autoscaling_group" "tf-asg" {
   launch_configuration = aws_launch_configuration.launchconfig.name
-  vpc_zone_identifier = [data.aws_subnets.default.ids]
+  vpc_zone_identifier  = [data.aws_subnets.default.ids]
 
   target_group_arns = [aws_lb_target_group.alb-TG.arn]
   health_check_type = "ELB"
@@ -66,18 +66,18 @@ data "aws_subnets" "default" {
 ## Create a load balancer
 
 resource "aws_lb" "server2-lb" {
-  name = "tf-server2-lb"
+  name               = "tf-server2-lb"
   load_balancer_type = "application"
-  subnets = data.aws_subnets.default.ids
-  security_groups = [aws_security_group.alb-SG.id]
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb-SG.id]
 }
 
 ## Define a listener for the ALB 
 
 resource "aws_lb_listener" "lb-lsnr" {
   load_balancer_arn = aws_lb.server2-lb.arn
-  port = 80
-  protocol = "HTTP"
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
     type = "fixed-response"
@@ -85,7 +85,7 @@ resource "aws_lb_listener" "lb-lsnr" {
     fixed_response {
       content_type = "text/plain"
       message_body = "404: page not found"
-      status_code = 404
+      status_code  = 404
 
     }
   }
@@ -94,18 +94,18 @@ resource "aws_lb_listener" "lb-lsnr" {
 # Create a target group for the auto scaling group
 
 resource "aws_lb_target_group" "alb-TG" {
-  name = tf-alb-TG
-  port = var.server_port
+  name     = tf-alb-TG
+  port     = var.server_port
   protocol = "HTTP"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id   = data.aws_vpc.default.id
 
   health_check {
-    path = "/"
-    protocol = "HTTP"
-    matcher = "200"
-    interval = 15
-    timeout = 3
-    healthy_threshold = 2
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
     unhealthy_threshold = 2
   }
 }
@@ -114,26 +114,26 @@ resource "aws_lb_target_group" "alb-TG" {
 
 resource "aws_lb_listener_rule" "lb-lsnr-rule" {
   listener_arn = aws_lb_listener.lb-lsnr.arn
-  priority = 100
+  priority     = 100
 
   condition {
     path_pattern {
-        values = ["*"]
+      values = ["*"]
     }
   }
 
   action {
-    type = "forward"
-    target_group_arn = aws_lb_target_group.alb-TG.arn 
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb-TG.arn
   }
 }
 resource "aws_security_group" "alb-SG" {
   name = "tf-alb-SG"
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
