37a17ac03059642448eb157c04d526846743ea27
TheGozie <chiggy.nm@gmail.com>
2023-01-05 21:46:01 +0100

    Added a load balancer and it's dependencies, added an ASG and dependencies and cleaned up the code

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,4 @@
-output "server2-publicIP" {
-  description = "This is the public IP address of the web server."
-  value       = aws_instance.server2.public_ip
-
+output "alb-dns-name" {
+  description = "This is the Domain name of the load balancer."
+  value       = aws_lb.server2-lb.dns_name
 }
